3536dc2b2d046557ad7cf9a9a70d9eaae827d386
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-16 07:00:11 +0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def about
-  	@about_me = "Hi there, William here....I have developed interest in data analysis, quantitative research, and using various statistical techniques in order to develop useful solutions to problems I come across. A result driven individual with great communication skills that can be a valuable asset to any working team. In search of opportunities that will enable me gain practical experience on the field of Statistics while developing my career path"
+  	@about_me = "Hi there, William here..."
   	
 
 
