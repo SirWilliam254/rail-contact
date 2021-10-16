613a83c06eb000cb7987cf2ab41b01bc90b1fc48
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-16 06:00:49 +0300

    Add files via upload

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,77 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+  end
+
+  
+  def new
+    
+    @friend = current_user.contacts.build
+  end
+
+
+  def edit
+  end
+
+  def create
+    
+    @contact = current_user.contacts.build(contact_params)
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ 
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+        format.json { render :show, status: :ok, location: @contact }
+      else
+        format.html { render :edit }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def correct_user
+    @contact = current_user.contacts.find_by(id: params[:id])
+    redirect_to contacts_path, notice: "Not Authorized To Edit This Contact" if @contact.nil?
+  end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def friend_params
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
+    end
+end
